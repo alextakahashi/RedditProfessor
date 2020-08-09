2919218e18fe319971f4d685f4b27383a2b3b041
Tejesh <tejeshb2@illinois.edu>
2020-08-09 14:47:12 -0700

    Basic bot will make changes

--- reddit-bot.py
+++ reddit-bot.py
@@ -7,7 +7,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # TODO: initialize the reddit bot
-def initializeBot():
-    # bot = praw.Reddit()
+def main():
+    print("Ce")
+    reddit = praw.Reddit(client_id = "4Fmc8A0V4diHiw", 
+                         client_secret = "qAMunNBhqkNGGWMuz57Z7GA3K6E",
+                         username = "HackThisBot",
+                         password = "shadowaaries1752",
+                         user_agent = "RedditProfessor")
+    
+    subreddit = reddit.subreddit("memes")
+    keyphrase = "!prof"
+    
+    for comment in subreddit.stream.comments():
+        try :
+            if (keyphrase in comment.body) :
+                comment.reply("yay")
+                print("Sd")
+        except :
+            print("tried")   
+        
+
+    
 
     return
+
+"""
+if __name__ == '__initializeBot__':
+    initializeBot() 
+"""
+    
+
+if __name__ == '__main__':
+    main()
