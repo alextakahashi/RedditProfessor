a3b70eead82cd479575a8ccc6a97f889094cbd3b
Aniruddha Pai <agpai2@illinois.edu>
2020-08-09 11:17:12 +0400

    Created file for the implementation of reddit bot

---
+++ reddit-bot.py
@@ -0,0 +1,13 @@
+import praw
+import os
+import time
+
+# Using .env file to read environmental variables
+from dotenv import load_dotenv
+load_dotenv()
+
+# TODO: initialize the reddit bot
+def initializeBot():
+    # bot = praw.Reddit()
+
+    return
